787529fb603efa39e2fe3fd7e2f438943ac81446
taingk <taing_k@etna-alternance.net>
2019-06-23 20:19:24 +0200

    init with Provider, eslint + prettier + husky, react router dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Provider from './components/Provider';
+import Home from './pages';
+import Nav from './components/Nav';
+
+const AppRouter = () => (
+  <Provider>
+    <Router>
+      <Nav />
+
+      <Route path="/" exact component={Home} />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
